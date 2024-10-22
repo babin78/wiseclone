56fc9302321e7925dc71590d088b603a9dd6f061
Babin78 <arijit.newgen@gmail.com>
2024-10-22 18:09:00 +0530

    abc

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import  { useState} from "react";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import MenuPopUP from "../Popup/MenuPopUP";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/favicon.webp";
+
 
 import "./style.scss";
 
@@ -12,6 +13,9 @@ const Header = () => {
 
   const [showPopup, setShowPopup] = useState(false);
   const [showRegister, setRegister] = useState(true);
+
+  
+  
 
   const openPopup = () => {
     setShowPopup(true);
@@ -30,15 +34,68 @@ const Header = () => {
           <img src={logo} alt="" />
         </div>
 
+        <ul className="navList">
+              <li className="navItem">
+                <a className="navBTN active" href="/">
+                  Personal
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navBTN" href="/">
+                  Bussiness
+                </a>
+              </li>
+              <li className="navItem">
+                <a className="navBTN" href="/">
+                  Platform
+                </a>
+              </li>
+            </ul>
+
+
+
+        <ul className="menuItems">
+        <li className="menuItem">
+           
+              <div className="toolTip">Features
+              <div className="tooltiptext">
+              <span>Send Money</span> <br />
+                  <p>
+                    Make a one-off payment. You&apos;ll get the real exchange rate
+                    with the low fee we&apos;re known for. Send large amounts
+                  </p>
+                  <br />
+                  <span>Send large amount</span>
+                
+              </div>
+
+              </div>
+              
+            
+          </li>
+          <li className="menuItem"> <a  href="">
+                  Priceing
+                </a></li>
+                <li className="menuItem"> <a  href="">
+                  Help
+                </a></li>
+        
+        </ul>
+            
+            
         <div className="lineMenu">
           {showRegister && <button className="registerBtn">Register</button>}
-
           {showPopup ? (
-            <VscChromeClose onClick={closePopup} />
-          ) : (
-            <SlMenu onClick={openPopup} />
-          )}
+                  <VscChromeClose onClick={closePopup} />
+                ) : (
+                  <SlMenu onClick={openPopup} />
+                )
+              }
+
+         
         </div>
+
+        
       </header>
       {showPopup && <MenuPopUP show={showPopup} />}
     </ContentWrapper>
--- src/pages/home/section2/Section2.jsx
+++ src/pages/home/section2/Section2.jsx
@@ -7,10 +7,12 @@ const Section2 = () => {
   return (
     <ContentWrapper>
       <div className="section2">
-        <ReactPlayer
+      <ReactPlayer
           className="vdPlayer"
-          url="https://www.youtube.com/watch?v=7whUKWQ-uSg"
+          url="https://wise.com//static-assets/app/_next/static/media/3d-globe-cfe618f7f706583422789cd9773a062a.mp4"
           playing={true}
+          controls={true }
+          muted={true}
           loop={true}
         />
       </div>
